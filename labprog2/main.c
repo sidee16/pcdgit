b711e6cfa58ef6e8c2c48ba62aab1f2bcb4a83ab
sudhir kumar raju <sukr16ee@cmrit.ac.in>
2016-09-12 22:02:24 +0530

    Update main.c

--- labprog2/main.c
+++ labprog2/main.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 int main ()
 {
-int a, rev=0;
+int a, rev=0,temp;
 printf("enter the no to reverse\n");
 scanf("%d", &a);
- 
-while(a != 0)
+temp=a; 
+while(temp != 0)
       { 
-       rev = rev*10;
-       rev = rev + a%10;
-       a   = a/10;
+       rev = (rev*10);
+       rev = (rev + (temp%10));
+       temp   = (temp/10);
       }
  printf("reverse of the no is = %d\n", rev);
          if(a == rev){
