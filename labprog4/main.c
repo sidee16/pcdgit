99695691d8b08d3491b677cd78eae3496e071a45
sudhir kumar raju <sukr16ee@cmrit.ac.in>
2016-09-13 18:22:57 +0530

    Update main.c

--- labprog4/main.c
+++ labprog4/main.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
-int ly(int y);
+int leap(int y);
 
- int ly(y)
+ int leap(y)
  {
     if(((y%4==0)&&(y%100!=0))||(y%400==0))
     return 0;
@@ -11,15 +11,15 @@ int ly(int y);
 
 int main()
 {
-  int x ,y;
+  int year ,r;
   printf("enter the year  :");
-  scanf("%d",&y);
-  x=ly(y);
-  if(x==0)
+  scanf("%d",&year);
+  x=leap(year);
+  if(r==0)
   {
     printf("it is a leap year\n");
   }
-  else if(x==1)
+  else if(r==1)
   {
     printf("it is not a leap year\n");
   }
