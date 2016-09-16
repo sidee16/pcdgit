d85f3df8e3536b076a5efcbe6e5ccafcd977ae32
sudhir kumar raju <sukr16ee@cmrit.ac.in>
2016-09-16 12:25:31 +0530

    Update main.c
    bubble

---
+++ labprog7/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+int a[100],swap, i,k,n;
+
+printf("enter the no of elements\n");
+scanf("%d",&n);
+
+for(i=0;i<n;i++)
+  {
+  printf("a[%d]= ",i);
+  scanf("%d", &a[i]);
+      }
+
+  for(i=0; i<n; i++)
+  {
+     for(k=0; k<=n-1-i; k++)
+     {
+        if(a[k]> a[k+1])
+        {
+          swap = a[k];
+          a[k] = a[k+1];
+          a[k+1] = swap;
+
+        }
+
+     }
+
+  }
+
+  printf("sorted list in ascending order is\n");
+  for(i=0;i<n;i++)
+     printf("%d\n",a[i]);
+return 0;
+
+
+}
