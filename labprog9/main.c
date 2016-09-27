ca47a08772a6aa494babf39b5262a70798f9a9f9
sudhir kumar raju <sukr16ee@cmrit.ac.in>
2016-09-28 00:09:15 +0530

    Update main.c

--- labprog9/main.c
+++ labprog9/main.c
@@ -1,68 +1,45 @@
 #include <stdio.h>
-int matrix(int m , int n, int p, int q, int a[m][n] , int b[p][q] )
+#include <stdlib.h>
 
+int matrix(int m, int n, int a[m][n], int p, int q, int b[p][q], int c[m][q])
 {
    int i,j,k;
 
-       printf("enter the matrices a(m*n) \n");
-         for(i=0 ; i<m ; i++)
-               {
-                   for(j=0 ; j<n ; j++)
-                        {
-                            printf("a[%d][%d]\n",i,j);
-                             scanf("%d",&a[i][j]);
-                         }
-                }
-
-      printf("enter the matrices b(p*q) \n");
-         for(i=0 ; i<p ; i++)
-               {
-                   for(j=0 ; j<q ; j++)
-                        {
-                            printf("b[%d][%d]\n",i,j);
-                             scanf("%d",&b[i][j]);
-                         }
-                 }
-
-                 int c[100][100];
-
-       for(i=0 ; i<m ; i++)
-           {
-               for(j=0 ; j<q ; j++)
-                   {
-                         c[i][j]=0;
-                      for(k=0 ; k<n ; k++)
-                           {
-                                  c[i][j]= (c[i][j] + (a[i][k] * b[k][j]));
-                            }
-                    }
+   for(i=0 ; i<m ; i++)
+    {
+      for(j=0 ; j<q ; j++)
+        {
+        c[i][j]=0;
+          for(k=0 ; k<n ; k++)
+            {
+                c[i][j]= (c[i][j] + (a[i][k] * b[k][j]));
             }
+         }
+     }
 
- for(i=0 ; i<m ; i++)
-           {
-               for(j=0 ; j<q ; j++)
-                   {
-                      printf("c[%d][%d]=%d",i,j,c[i][j]);
-                      printf("\n");
-                   }
-           }
+for(i=0 ; i<m ; i++)
+    {
+        for(j=0 ; j<q ; j++)
+            {
+              printf("c[%d][%d]=%d",i,j,c[i][j]);
+            printf("\n");
+            }
+    }
 
 return c[i][j];
 }
 
 
-
-
 int main()
 {
-  int a[100][100],b[100][100];
-  int m,n,p,q,r;
+int m,n,p,q,r,i,j;
 
 printf("enter the element of matric a(m*n)m=,n=\n");
 scanf("%d%d",&m,&n);
 printf("enter the element of matric B(p*q)p=,q=\n");
 scanf("%d%d",&p,&q);
 
+int a[m][n],b[p][q],c[m][q];
 
  if(n!=p)
     {
@@ -70,10 +47,36 @@ scanf("%d%d",&p,&q);
       }
   else {
 
-    r=matrix (m,n,p,q,a,b);
+       printf("enter the matrices a(m*n) \n");
+            for(i=0 ; i<m ; i++)
+               {
+                  for(j=0 ; j<n ; j++)
+                    {
+                      printf("a[%d][%d]\n",i,j);
+                       scanf("%d",&a[i][j]);
+                    }
+                }
 
-  }
-    printf("new matrix is %d", r);
+      printf("enter the matrices b(p*q) \n");
+         for(i=0 ; i<p ; i++)
+               {
+                 for(j=0 ; j<q ; j++)
+                    {
+                     printf("b[%d][%d]\n",i,j);
+                      scanf("%d",&b[i][j]);
+                    }
+               }
+
+
+r=matrix (m,n,a,p,q,b,c);
+
+    printf("new matrix is ");
+    return r;
+
+
+      }
+
 
 return 0;
+
 }
