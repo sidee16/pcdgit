0108173cc4f285caa5a4aa451f2ae7f6fcf87e96
sudhir kumar raju <sukr16ee@cmrit.ac.in>
2016-09-27 22:10:45 +0530

    Update main.c

--- labprog9/main.c
+++ labprog9/main.c
@@ -1,51 +1,4 @@
 #include <stdio.h>
-
-
-int main()
-{
-  int a[100][100],b[100][100];
-  int m,n,p,q,r;
-
-printf("enter the element of matric a(m*n)m=,n=\n");
-scanf("%d%d",&m,&n);
-printf("enter the element of matric B(p*q)p=,q=\n");
-scanf("%d%d",&p,&q);
-
-int i,j;
-       printf("enter the matrices a(m*n) \n");
-         for(i=0 ; i<m ; i++)
-               {
-                   for(j=0 ; j<n ; j++)
-                        {
-                            printf("a[%d][%d]\n",i,j);
-                             scanf("%d",&a[i][j]);
-                         }
-                }
-
-      printf("enter the matrices b(p*q) \n");
-         for(i=0 ; i<p ; i++)
-               {
-                   for(j=0 ; j<q ; j++)
-                        {
-                            printf("b[%d][%d]\n",i,j);
-                             scanf("%d",&b[i][j]);
-                         }
-                 }
- if(n!=p)
-    {
- printf("can't multiply wrong input!!! try again \n");
-      }
-  else {
-
-    r=matrix (m,n,p,q,a,b);
-
-  }
-    printf("new matrix is %d", r);
-
-return 0;
-}
-
-
 int matrix(int m , int n, int p, int q, int a[m][n] , int b[p][q] )
 
 {
@@ -77,3 +30,52 @@ int matrix(int m , int n, int p, int q, int a[m][n] , int b[p][q] )
 
 return c[i][j];
 }
+
+
+int main()
+{
+  int a[100][100],b[100][100];
+  int m,n,p,q,r;
+
+printf("enter the element of matric a(m*n)m=,n=\n");
+scanf("%d%d",&m,&n);
+printf("enter the element of matric B(p*q)p=,q=\n");
+scanf("%d%d",&p,&q);
+
+
+ if(n!=p)
+    {
+ printf("can't multiply wrong input!!! try again \n");
+      }
+  else {
+
+  int i,j;
+       printf("enter the matrices a(m*n) \n");
+         for(i=0 ; i<m ; i++)
+               {
+                   for(j=0 ; j<n ; j++)
+                        {
+                            printf("a[%d][%d]\n",i,j);
+                             scanf("%d",&a[i][j]);
+                         }
+                }
+
+      printf("enter the matrices b(p*q) \n");
+         for(i=0 ; i<p ; i++)
+               {
+                   for(j=0 ; j<q ; j++)
+                        {
+                            printf("b[%d][%d]\n",i,j);
+                             scanf("%d",&b[i][j]);
+                         }
+                 }
+
+    r=matrix (m,n,p,q,a,b);
+
+  }
+    printf("new matrix is %d", r);
+
+return 0;
+}
+
+
